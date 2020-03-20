75078ad182f7d9552ae8b051f63a7270fb8634d4
josiah <josiahdamiwilliams@gmail.com>
2020-03-20 15:42:30 +0100

    Added new files to separate route for task and resources also edited …
    …package.json to include server for nodemon

--- router/project-route-helper.js
+++ router/project-route-helper.js
@@ -1,25 +1,22 @@
 const db = require("../data/db-config");
+const mapper = require ("../router/mapper")
+const resource = require('./resource-route-helper')
+const task = require('./task-route-helper')
+
 
 module.exports = {
-    getProjects,
     addProject,
-    getProjectById,
-    addTask, 
-    getProjectTasks,
-    findResources,
-    addResource,
-    getTasks,
-    get,
-    findResourcesById,
+    getprojects
+   
 } 
 
-function get(id) {
-    let query = db('projects as p');
+function getprojects(id) {
+    let query = db('projects');
 
     if (id) {
-      query.where('p.id', id).first();
+      query.where('projects.id', id).first();
 
-      const promises = [query, this.getProjectTasks(id)];  this.findResourcesById(id)// [ projects, tasks ]
+      const promises = [query, tasks.getProjectTasks(id)];  resource.findLinkedResourcesById(id)// [ projects, resources ]
 
       return Promise.all(promises).then(function(results) {
         let [project, tasks, resource] = results;
@@ -40,46 +37,7 @@ function get(id) {
       return projects.map(project => mappers.projectToBody(project));
     });
 }
-function getProjectById (id) {
-    return db("projects").where({id}).first()
-}
 
-function getProjects () {
-    return db("projects");
-}
-
-function getTasks () {
-    return db("task")
-    .then(tasks => tasks.map(task => mappers.actionToBody(task)));
-}    
 function addProject (project) {
     return db("projects").insert(project)
 }
-
-function addTask (task) {
-    return db("task").insert(task)
-}
-function findResources  () {
-    return db("resource");
-}
-
-function findResourcesById (id) {
-    return db('project_resource')
-    .join('resource', 'resource.id', 'project_resource.resource_id')
-    .join('projects', 'projects.id', 'project_resource.project_id')
-    .select('resource.id', 'resource.name', 'resource.description')
-    .where('projects.id', id);
-}
-
-function addResource (resource) {
-    return db("resource").insert(resource)
-}
-
-function getProjectTasks (id) {
-    return db("task")
-        .join("projects", "projects.id", "task.project_id")
-        .where({project_id: id})
-        .select("task.id", "projects.name", "projects.description", "task.task.description", "task.notes", "task.completed", )
-        .then(tasks => tasks.map(task => mappers.actionToBody(task)));
-
-} 
--- router/project-route.js
+++ router/project-route.js
@@ -6,7 +6,7 @@ const db = require("./project-route-helper");
 
 //get Requests
 router.get("/projects", (req, res) => {
-    db.get()
+    db.getprojects()
         .then(projects => {
             res.status(200).json(projects)
         })
@@ -16,7 +16,7 @@ router.get("/projects", (req, res) => {
 })
 
 router.get("/projects/:id", (req, res) => {
-    db.get(req.params.id)
+    db.getprojects(req.params.id)
         .then(project => {
             res.status(200).json(project)
         })
@@ -25,45 +25,7 @@ router.get("/projects/:id", (req, res) => {
         })
 })
 
-router.get("/tasks", (req, res) => {
-    db.getTasks()
-        .then(projects => {
-            res.status(200).json(projects)
-        })
-        .catch(error => {
-            res.status(500).json({ message: "Something went wrong:-" + error.message})
-        })
-})
 
-router.get("/taskOnProject/:id", (req, res) => {
-    db.getProjectTasks(req.params.id)
-        .then(tasks => {
-            res.status(200).json(tasks)
-        })
-        .catch(error => {
-            res.status(500).json({ message: "We cannot retrieve those tasks at this time. " + error.message})
-        })
-})
-
-router.get("/resources", (req, res) => {
-    db.findResources()
-        .then(resources => {
-            res.status(200).json(resources)
-        })
-        .catch(error => {
-            res.status(500).json({ message: "The resources you are looking for cannot be retrieved at this time:-" + error.message})
-        })
-})
-
-router.get("/resources/:id", (req, res) => {
-    db.findResourcesById(req.params.id)
-        .then(resources => {
-            res.status(200).json(resources)
-        })
-        .catch(error => {
-            res.status(500).json({ message: "The resources you are looking for cannot be retrieved at this time:-" + error.message})
-        })
-})
 
 //post requests
 router.post("/projects", (req, res) => {
@@ -78,32 +40,6 @@ router.post("/projects", (req, res) => {
                 res.status(500).json({ message: "something went wrong:-. " + error.message})
             })
     }
-router.post("/tasks", (req, res) => {
-    if(!req.body.description && !req.body.project_id){
-        res.status(400).json({message: "Please provide needed column for the post namely 'description' and 'project_id' "})
-    }else{
-        db.addTask(req.body)
-            .then(tasks => {
-                    res.status(200).json({message: `New project with ID ${tasks} got created`})
-            })
-            .catch(error => {
-                res.status(500).json({ message: "something went wrong:-. " + error.message})
-            })
-    }
-})
-router.post("/resources", (req, res) => {
-    if(!req.body.name){
-        res.status(400).json({message: "Please provide needed column for the post 'name' "})
-    }else{
-        db.addResource(req.body)
-            .then(resource => {
-                    res.status(200).json({message: `New project with ID ${resource} got created`})
-            })
-            .catch(error => {
-                res.status(500).json({ message: "something went wrong:-. " + error.message})
-            })
-    }
-})
 
 
 
